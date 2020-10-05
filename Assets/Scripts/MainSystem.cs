9464f3a494875a4fe06b2eb4de8dc4d866c6411d
tetsuomi <kondo.tetta851@mail.kyutech.jp>
2020-10-05 18:28:24 +0900

    Generate block when space button is pushed

--- Assets/Scripts/MainSystem.cs
+++ Assets/Scripts/MainSystem.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class MainSystem : MonoBehaviour
 {
+    public float nextX = 0;
+    public float intervalX = 1.0f;
     public GameObject target;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,7 +20,8 @@ public class MainSystem : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Instantiate( 生成するオブジェクト,  場所, 回転 );  回転はそのままなら↓
-            Instantiate(target, new Vector3(1.0f, 2.0f, 0.0f), Quaternion.identity);
+            Instantiate(target, new Vector3(nextX, 0, 0.0f), Quaternion.identity);
+            nextX += intervalX;
         }
 
     }
